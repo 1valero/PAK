7d4ab95df41b3bf50c3f0a26156f129cbc5adde7
amd90 <leo22_07@hotmail.com>
2018-04-26 00:17:40 -0400

    Dual xib single view demo

---
+++ pak/CategoriasCell.swift
@@ -0,0 +1,12 @@
+//
+//  CategoriasCell.swift
+//  pak
+//
+//  Created by user137834 on 4/10/18.
+//
+
+import UIKit
+
+class CategoriasCell: UICollectionViewCell {
+    
+}
---
+++ pak/ListaProductosVC.swift
@@ -0,0 +1,8 @@
+//
+//  ListaProductosVC.swift
+//  pak
+//
+//  Created by user137834 on 4/10/18.
+//
+
+import Foundation
